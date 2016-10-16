422da608bcf07b79a1312d81a683ac9d76e9c64f
Ivan Dobrovolsky <dobrovolsky94@gmail.com>
2016-10-17 01:24:33 +0300

    Implemented navigation component.

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,11 +1,11 @@
 var jQuery = require('jquery');
 
-// var navigationHandler = require('./handlers/navigation-handler');
+var navigationHandler = require('./handlers/navigation-handler');
 var scrollHandler = require('./handlers/scroll-handler');
 
 jQuery(document).ready(function () {
 
     scrollHandler(jQuery);
-    // navigationHandler(jQuery);
+    navigationHandler(jQuery);
 
 });
