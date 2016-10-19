f4f0a6abf4a48e81969768a70b0004ab31385846
Ivan Dobrovolsky <dobrovolsky94@gmail.com>
2016-10-19 22:49:01 +0300

    Implemented feedbacks section.

---
+++ src/assets/scripts/handlers/feedbacks-slider-handler.js
@@ -0,0 +1,15 @@
+var slick = require('slick-carousel-browserify');
+
+module.exports = function ($) {
+    $('.feedbacks .slick-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        infinite: true,
+        adaptiveHeight: true,
+        dots: true,
+        speed: 300
+    });
+
+    $('.feedbacks .slick-carousel .slick-dots button').text('');
+};
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,11 +3,14 @@ var jQuery = require('jquery');
 var navigationHandler = require('./handlers/navigation-handler');
 var scrollHandler = require('./handlers/scroll-handler');
 var googleMapsHandler = require('./handlers/google-maps-handler');
+var feedbacksSliderHandler = require('./handlers/feedbacks-slider-handler');
+
 
 jQuery(document).ready(function () {
 
     scrollHandler(jQuery);
     navigationHandler(jQuery);
     googleMapsHandler(jQuery);
+    feedbacksSliderHandler(jQuery);
 
 });
