ec9ae09998a2d5043913fc7969decf170478001c
Ivan Dobrovolsky <dobrovolsky94@gmail.com>
2016-10-24 01:40:16 +0300

    Added scrollHandler for cv page.

--- src/assets/scripts/main-cv.js
+++ src/assets/scripts/main-cv.js
@@ -1,7 +1,10 @@
 var jQuery = require('jquery');
 
 var cvNavigationScrollHandler = require('./handlers/cv-navigation-scroll-handler');
+var scrollHandler = require('./handlers/scroll-handler');
+
 
 jQuery(document).ready(function () {
     cvNavigationScrollHandler(jQuery);
+    scrollHandler(jQuery);
 });
