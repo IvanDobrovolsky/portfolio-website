b5a010a4e118c6b79b57646443c0050bd9de2365
Ivan Dobrovolsky <dobrovolsky94@gmail.com>
2016-10-26 18:14:43 +0300

    Removed arrows from projects slider.

--- src/assets/scripts/handlers/projects-slider-handler.js
+++ src/assets/scripts/handlers/projects-slider-handler.js
@@ -4,13 +4,11 @@ module.exports = function ($) {
     $('.projects .slick-carousel').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: true,
+        arrows: false,
         infinite: true,
         adaptiveHeight: true,
         dots: true,
         speed: 300
     });
 
-    $('.slick-prev').html('<i class="fa fa-arrow-left"></i> Previous project');
-    $('.slick-next').html('Next project <i class="fa fa-arrow-right"></i>');
 };
